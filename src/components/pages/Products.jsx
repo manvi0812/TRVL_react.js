d67c6bad05ddc6d60b7e89481f460909841c445c
unknown <59312850+manvi0812@users.noreply.github.com>
2021-04-06 18:49:08 +0530

    first commit

---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App.css";
+
+export default function Products() {
+  return (
+    <div className='products'>
+      <img src='/images/img-2.jpg' alt='' />
+      <h1>PRODUCTS</h1>
+    </div>
+  );
+}
