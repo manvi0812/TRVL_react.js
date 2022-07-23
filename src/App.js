e76f87b34fffa68f422080154b65ceae7143e312
Manvi <manvi@kawa.space>
2022-07-23 18:18:25 +0530

    deployed and fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route , Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './components/pages/Home';
 import Products from './components/pages/Products';
@@ -9,18 +9,17 @@ import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-      <>
-    <Router>
-    <Navbar/>
-    <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/services" component={Services}/>
-      <Route path="/products" component={Products}/>
-      <Route path="/sign-up" component={SignUp}/>
-    </Switch>
-    </Router>
+    <>
+      {/* <Router> */}
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+      {/* </Router> */}
     </>
-    
   );
 }
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,25 +1,21 @@
-import React from "react";
-import "../App.css";
-import { Button } from "./button";
-import "./HeroSection.css";
+import React from 'react';
+import '../App.css';
+import { Button } from './button';
+import './HeroSection.css';
+
+import video from '../videos/video-2.mp4';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <video src={video} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
-        <Button
-          className='btn'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'>
+        <Button className='btn' buttonStyle='btn--outline' buttonSize='btn--large'>
           GET STARTED
         </Button>
-        <Button
-          className='btn'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'>
+        <Button className='btn' buttonStyle='btn--primary' buttonSize='btn--large'>
           WATCH TRAILER <i className='fa fa-play-circle-o' />
         </Button>
       </div>
